1d2b2e39fc5506afb83f0215c6af1cb5a0f34d70
Gabriella Homem <gabriella.rbh@gmail.com>
2018-08-18 11:45:08 -0300

    Add views related to character selection screen

---
+++ IlegraChallenge/Views/Cell/CharacterCell.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterCell.swift
+//  IlegraChallenge
+//
+//  Created by Gabriella Barbieri on 18/08/18.
+//  Copyright © 2018 gabi. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
