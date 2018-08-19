443f2416e2d18876d6d85318d52e6cb676bcff91
Gabriella Homem <gabriella.rbh@gmail.com>
2018-08-19 11:29:28 -0300

    Update main coordinator to show details screen

--- IlegraChallenge/AppCoordinator.swift
+++ IlegraChallenge/AppCoordinator.swift
@@ -11,12 +11,26 @@ import UIKit
 
 class AppCoordinator: BaseCoordinator<Void> {
     private let window: UIWindow!
+    var didSelectCharacter = PublishSubject<Character>()
     
     init(window: UIWindow) {
         self.window = window
     }
     
     override func start() -> Observable<Void> {
+        let viewModel = CharactersViewModel(self)
+        let viewController = CharactersView.with(viewModel)
+        let navController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        
+        didSelectCharacter
+            .subscribe(onNext: { character in
+                let viewModel = CharactersDetailViewModel(self, character: character)
+                let viewController = CharacterDetailsView.with(viewModel)
+                navController.pushViewController(viewController, animated: true)
+            })
+            .disposed(by: disposeBag)
         
         return Observable.never()
     }
