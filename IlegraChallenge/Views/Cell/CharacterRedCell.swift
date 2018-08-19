a3ff39235f0f0f35c56b1acea0b1ea353e532c56
Gabriella Homem <gabriella.rbh@gmail.com>
2018-08-19 12:54:09 -0300

    Add first view assets

---
+++ IlegraChallenge/Views/Cell/CharacterRedCell.swift
@@ -0,0 +1,24 @@
+//
+//  CharacterRedCell.swift
+//  IlegraChallenge
+//
+//  Created by Gabriella Barbieri on 19/08/18.
+//  Copyright © 2018 gabi. All rights reserved.
+//
+
+import UIKit
+
+class CharacterRedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
